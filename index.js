ad78a0bb0a0bc9bd320175f1bbd43f135fac47cf
Robert Gebhardt <robert.gol.jr@gmail.com>
2016-11-27 13:04:01 -0200

    Adicionando permissão para CORS

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express')
+var fs = require('fs')
 var app = express()
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  fs.readFile('/proc/meminfo', (err, data) => {
+    if (err) throw err;
+    res.send(data)
+  })
 })
 
 app.listen(3000, function () {
